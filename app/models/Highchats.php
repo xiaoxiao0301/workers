6d7cc3e2f203315083e76fb8a6d0b0c7963d122f
jixiaoxiao <jixiaoxiao0301@163.com>
2020-04-02 16:37:55 +0800

    redis zsh

---
+++ app/models/Highchats.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Highchats extends Model
+{
+    use SoftDeletes;
+}
